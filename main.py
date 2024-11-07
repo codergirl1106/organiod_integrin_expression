f2303be1de114d1c0ca118850d8c04d9fb18e002
codergirl1106 <68753302+codergirl1106@users.noreply.github.com>
2024-11-07 17:26:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import subprocess
+
+def my_cache(f):
+    @st.cache_data(max_entries=5, ttl=600)
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        return f(*args, **kwargs)
+    return inner
+
+@st.cache_data
+def umap_plot_func(umap_data, colorby):
+    return px.scatter(umap_data, x="umap_1", y="umap_2", color=colorby)
+
+@st.cache_data
+def load_data(rds):
+    rds2 = rds.getvalue()
+    with open("rds_file.rds", 'wb') as w:
+        w.write(rds2)
+        
+    process = subprocess.Popen(["Rscript", "organiod_integrin_expression.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result = process.communicate()
+
+def main():    
+    rds = st.file_uploader("upload an .rds file")
+
+    if rds != None:
+
+        load_data(rds)
+
+        umap_data = pd.read_csv("umap_data.csv", index_col=0)
+
+        integrin_expression_data = pd.read_csv("integrin_expression.csv", index_col=0)
+
+        colorby_options = list(umap_data.columns)
+        colorby_options.remove('nCount_RNA')
+        colorby_options.remove('nFeature_RNA')
+        colorby_options.remove('umap_1')
+        colorby_options.remove('umap_2')
+        colorby_options.remove('cell_type')
+        colorby_options = ['cell_type']+colorby_options
+        
+        colorby = st.selectbox('', colorby_options)
+
+        umap_plot = st.plotly_chart(umap_plot_func(umap_data, colorby), key="umap", on_select="ignore")
+        
+        st.dataframe(integrin_expression_data)
+
+main()
---
+++ organiod_integrin_expression.R
@@ -0,0 +1,33 @@
+library(Seurat)
+library(dplyr)
+library(ggplot2)
+
+hi <- readRDS("rds_file.rds")
+
+hi <- hi %>% 
+  NormalizeData() %>% 
+  FindVariableFeatures() %>% 
+  ScaleData() %>% 
+  RunPCA() %>% 
+  RunUMAP(dims = 1:30)
+
+umap_coords <- hi@reductions$umap@cell.embeddings
+umap_coords <- merge(umap_coords, hi@meta.data, by="row.names")
+write.csv(umap_coords, file="umap_data.csv", row.names = FALSE)
+
+tbll <- data.frame()
+genes <- c("ENSG00000213949", "ENSG00000164171", "ENSG00000005884", "ENSG00000115232", "ENSG00000161638", "ENSG00000091409", "ENSG00000135424", "ENSG00000077943", "ENSG00000144668", "ENSG00000143127", "ENSG00000137809", "ENSG00000156886", "ENSG00000083457", "ENSG00000005844", "ENSG00000169896", "ENSG00000138448", "ENSG00000005961", "ENSG00000140678", "ENSG00000150093", "ENSG00000160255", "ENSG00000259207", "ENSG00000132470", "ENSG00000082781", "ENSG00000115221", "ENSG00000139626", "ENSG00000105855", "ENSG00000105329", "ENSG00000092969", "ENSG00000119699", "ENSG00000049323", "ENSG00000119681", "ENSG00000168056", "ENSG00000090006", "ENSG00000137507", "ENSG00000174004")
+for (celltypes in unique(hi@meta.data$cell_type)) {
+  subsection <- hi@assays$RNA@counts[genes,rownames(hi@meta.data[hi@meta.data$cell_type == celltypes,])]
+  print(rowSums(subsection)/ncol(subsection))
+  if (ncol(tbll) == 0) {
+    tbll <- data.frame(cc = rowSums(subsection)/ncol(subsection))
+    colnames(tbll) <- celltypes
+  } else {
+    tbll[[celltypes]] <- rowSums(subsection)/ncol(subsection)
+  }
+}
+
+rownames(tbll) <- c("ITGA1", "ITGA2", "ITGA3", "ITGA4", "ITGA5", "ITGA6", "ITGA7", "ITGA8", "ITGA9", "ITGA10", "ITGA11", "ITGAD", "ITGAE", "ITGAL", "IGTAM", "ITGAV", "ITGA2B", "ITGAX", "ITGB1", "ITGB2", "ITGB3", "ITGB4", "ITGB5", "ITGB6", "ITGB7", "ITGB8", "TGFB1", "TGFB2", "TGFB3", "LTBP1", "LTBP2", "LTBP3", "LTBP4", "LRRC32", "NRROS")
+
+write.csv(tbll, file="integrin_expression.csv")
