90cadceff2608456319a1170294a5f4d752e418f
codergirl1106 <68753302+codergirl1106@users.noreply.github.com>
2024-11-07 21:34:10 -0500

    main.py

--- main.py
+++ main.py
@@ -16,32 +16,23 @@ def umap_plot_func(umap_data, colorby):
 
 @st.cache_data
 def load_data(rds):
-    print(0)
     rds2 = rds.getvalue()
     with open("rds_file.rds", 'wb') as w:
         w.write(rds2)
 
-    print(1)
     process = subprocess.Popen(["Rscript", "./organiod_integrin_expression.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print(2)
     result = process.communicate()
-    print(result)
 
 def main():
-    print("hi")
     rds = st.file_uploader("upload an .rds file")
 
     if rds != None:
-        print("hi2")
         load_data(rds)
-        print("hi3")
         umap_data = pd.read_csv("./umap_data.csv", index_col=0)
 
         integrin_expression_data = pd.read_csv("./integrin_expression.csv", index_col=0)
 
         colorby_options = list(umap_data.columns)
-        colorby_options.remove('nCount_RNA')
-        colorby_options.remove('nFeature_RNA')
         colorby_options.remove('umap_1')
         colorby_options.remove('umap_2')
         colorby_options.remove('cell_type')
